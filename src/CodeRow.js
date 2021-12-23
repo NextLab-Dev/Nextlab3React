44272339b5dd93f633401fc8051e9cc62f052d5b
Jelena Ilic <jelenanextlab3@gmail.com>
2021-12-23 15:14:19 +0100

    First two rows

--- src/CodeRow.js
+++ src/CodeRow.js
@@ -4,15 +4,15 @@ const CodeRow = () => {
     return (
       <div className="container-fluid g-0 codeRowContainer">
         <div className="row justify-content-center">
-          <div className="col-3">
+          <div className="col-9 col-sm-7 col-md-5 col-lg-4">
             <div className="paragraphStyle">
-              <p>Generate your:</p>
+              <p className="paragraphBold">Generate your:</p>
               <p>Vouchers <i className="bi bi-arrow-right"></i> Tokens <i className="bi bi-arrow-right"></i> Rewards</p>
               <p>and distribute them in your</p>
               <p>customers wallet</p>
             </div>
           </div>
-          <div className="col-4">
+          <div className="col-9 col-sm-7 col-md-5 col-lg-4 col-xl-3">
             <img src={codeImg} alt="Code" className="mockupStyle" />
           </div>
         </div>
--- src/NavbarRow.js
+++ src/NavbarRow.js
@@ -1,80 +1,64 @@
 const NavbarRow = () => {
-    const office = require("./img/darkOffice.jpg");
-    return (
-      <div className="imageContainer">
-        <img src={office} autoPlay loop playsInline muted></img>
-        <div className="insideObjectFirst">
-          <nav className="navbar navbar-dark navbar-expand-sm">
-            <h1 className="titleStyle">Nextlab 3</h1>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-  
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <a className="nav-link linkStyle" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link linkStyle" href="#">
-                    Teams
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link linkStyle" href="#">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-  
-            {/* <div className="dropdown">
-                <button
-                  className="btn btn-secondary dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown button
-                </button>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuButton"
-                >
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </div> */}
-          </nav>
-        </div>
-  
-        <div className="insideObjectSecond">
-          <p>Ideas Focused on</p>
-          <p>Blockchain, Cryptocurrency and</p>
-          <p>Wallets Development</p>
-        </div>
+//   const office = require("./img/darkOffice.jpg");
+  const juneoVideo = require("./img/landingVideo.mp4");
+  return (
+    <div className="imageContainer">
+      <video src={juneoVideo} autoPlay loop playsInline muted></video>
+      <div className="insideObjectFirst">
+        <nav className="navbar navbar-expand-sm navbar-dark">
+          <h1 className="companyName">
+            Nextlab 3
+          </h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Teams
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              {/* <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li> */}
+            </ul>
+          </div>
+        </nav>
       </div>
-    );
-  };
-  
-  export default NavbarRow;
-  
+
+      <div className="insideObjectSecond">
+        <p>Ideas Focused on</p>
+        <p>Blockchain, Cryptocurrency and</p>
+        <p>Wallets Development</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarRow;
