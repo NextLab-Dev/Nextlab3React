c14bc7f77e3b69d7461f5cd3fedc8558d060d248
NextLab-Dev <nextlabdev@gmail.com>
2022-01-19 11:11:44 +0100

    Changed nav imagesChanged nav images

--- src/NavbarContact.js
+++ src/NavbarContact.js
@@ -1,10 +1,10 @@
 import Navigation from "./Navigation";
 
 const NavbarContact = () => {
-  const contactImg = require("./img/Contact.jpg");
+  const officeImg = require("./img/Office.jpg");
   return (
     <header className="imageContainer">
-      <img src={contactImg} alt="Team" className="navImageStyle" />
+      <img src={officeImg} alt="Team" className="navImageStyle" />
       <div className="insideObjectFirst">
         <Navigation />
       </div>
