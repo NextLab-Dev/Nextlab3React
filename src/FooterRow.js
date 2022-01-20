6ada73fc37f4440e9516b353084365058fb85adf
NextLab-Dev <nextlabdev@gmail.com>
2022-01-20 10:31:43 +0100

    Copyright changed

--- src/FooterRow.js
+++ src/FooterRow.js
@@ -5,7 +5,7 @@ const FooterRow = () => {
         <div className="container-fluid footerRowContainer">
             <div className="row justify-content-center">
                 <div className="col-9 col-sm-7 col-md-5 col-lg-4 footerStyle">
-                    <p className="mb-1">Copyright &copy; 2021 Powered by Nextlab 3</p>
+                    <p className="mb-1">Copyright &copy; 2022 Powered by Nextlab 3</p>
                     <p className="mb-0">Made with <img src={serbiaFlag} alt="Serbia Flag" /> in Serbia</p>
                 </div>
             </div>
